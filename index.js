2f5408d3a2d127f29aecc99cd7d637b26405fe26
nisholin <61735805+nisholin@users.noreply.github.com>
2020-11-13 15:32:49 +0530

    save changes

--- index.js
+++ index.js
@@ -27,16 +27,29 @@ app.get('/api/users/:qrcode',(request, response)=>{
 app.post('/api/users/save',(req , res)=>{
     console.log(req.body);
   var postData  = req.body;
-      pool.query('INSERT INTO fooddetails SET ?', postData,function (err, result) {
-        if (err) throw err;
-        console.log("1 record inserted");
-        res.end(JSON.stringify(result));
-      });
+  var data=postData.length;
+for(var i=0;i<data;i++)
+{
+  pool.query('INSERT INTO fooddetails SET ?', postData[i],function (err, result) {
+    if (err) throw err;
+    
+    res.end(JSON.stringify(result));
+  });
+}
+console.log(+i+" record inserted");
+}); 
+
+/* 
+app.post('/api/users/save',(req , res)=>{
+  console.log(req.body);
+var postData  = req.body;
+var sql = "INSERT INTO fooddetails (itemvalue,itemname, qty,empcode) VALUES ?";
+pool.query(sql,postData, function(err) {
+  if (err) throw err;
+  conn.end();
 });
-
-
-
-
+});
+ */
 
 
 
